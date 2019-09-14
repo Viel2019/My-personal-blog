40fc580364449d3d57d7ed461f40a0c1c408f95f
Viel2019 <mainavielina@gmail.com>
2019-09-14 11:53:26 +0300

    initial commit for my personal blog

---
+++ footer.php
@@ -0,0 +1,32 @@
+<div class="container-fluid" id="footersection">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <h2>social media</h2>
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-google"></a>
+            <a href="#" class="fa fa-linkedin"></a>
+            <a href="#" class="fa fa-youtube"></a>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <p style="color: #ffffff;">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                A architecto, commodi consectetur enim ipsa itaque libero
+                odit quo quos recusandae, repudiandae sequi suscipit
+                temporibus vero vitae. Aliquam atque id possimus!</p>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,115 @@
+<?php include  "Header.php"?>
+
+
+<div class="container-fluid" id="section1">
+        <h1>I am a software Engineer</h1>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Accusamus aliquid amet cum dolorum, eos et fugiat iusto laudantium
+            pariatur placeat, quaerat quas quia quibusdam ullam veritatis!
+            Accusantium dicta pariatur repellat.</p>
+    <a href="" class="btn btn-warning">More Information</a>
+</div>
+<div class="container">
+        <div class="row" id="section2">
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                <div class="">
+                    <img src="Images/elephant.jpg" alt=""class="img-thumbnail">
+                    <div class="caption">
+                        <p>Lorem ipsum dolor sit </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                <div class="">
+                    <img src="Images/elephant.jpg" alt=""class="img-thumbnail">
+                    <div class="caption">
+                        <p>Lorem ipsum dolor sit </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                <div class="">
+                    <img src="Images/elephant.jpg" alt=""class="img-thumbnail">
+                    <div class="caption">
+                        <p>Lorem ipsum dolor sit </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <div class="row" id="section2">
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <div class="">
+                <img src="Images/elephant.jpg" alt=""class="img-thumbnail">
+                <div class="caption">
+                    <p>Lorem ipsum dolor sit </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <div class="">
+                <img src="Images/elephant.jpg" alt=""class="img-thumbnail">
+                <div class="caption">
+                    <p>Lorem ipsum dolor sit </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <div class="">
+                <img src="Images/elephant.jpg" alt=""class="img-thumbnail">
+                <div class="caption">
+                    <p>Lorem ipsum dolor sit </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <h1>Learn to code the hard way.</h1>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Aliquid, autem beatae cupiditate
+                    distinctio earum explicabo, facere harum ipsum libero
+                    magnam magni neque nostrum quaerat sapiente sunt vero
+                    voluptate, voluptatem voluptates!</p>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <h1>Learn to code the hard way.</h1>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Aliquid, autem beatae cupiditate
+                    distinctio earum explicabo, facere harum ipsum libero
+                    magnam magni neque nostrum quaerat sapiente sunt vero
+                    voluptate, voluptatem voluptates!</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include  "footer.php"?>
+
